84868f0974de7f5572ac6a1719be4e267c39505b
AnoukDunu <52993706+AnoukDunu@users.noreply.github.com>
2026-06-14 11:16:26 +1000

    staging load init

--- src/load/load_staging.py
+++ src/load/load_staging.py
@@ -1,1 +1,7 @@
-from database import connection
+from database.connection import get_connection
+
+def load_staging(df):
+    conn = get_connection()
+
+    df.to_sql('staging_table', con=conn, if_exists='replace', index=False)
+    print("Data loaded into staging table successfully.")
